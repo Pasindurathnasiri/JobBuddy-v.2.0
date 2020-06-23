0b1482e82fa4f83559abb4bd6e82aec9bcbec50a
Pasindu Madhuranga <45681668+Pasindurathnasiri@users.noreply.github.com>
2020-06-23 12:08:30 +0530

    Update vtc_delete.php

--- vtc_delete.php
+++ vtc_delete.php
@@ -1,6 +1,8 @@
 <?php  
 	$connect = mysqli_connect("localhost", "root", "", "jobbuddy");
-	$sql = "DELETE FROM vtcenters WHERE ComRegID = '".$_POST["id"]."'";  
+	$sql = "DELETE FROM vtcenters WHERE vtcRegID = '".$_POST["id"]."'";  
+    echo $_POST["id"];
+
 	if(mysqli_query($connect, $sql))  
 	{  
 		echo 'Data Deleted';  
